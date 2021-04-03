89572fc6c5d3a601cea76b1b7c64fb7c1ffb11e7
EimantasVinciunas <78846024+EimantasV@users.noreply.github.com>
2021-04-03 19:09:28 +0300

    0.5

--- Funkcijos.h
+++ Funkcijos.h
@@ -1,4 +1,6 @@
 #include<vector>
+#include<list>
+#include<deque>
 #include"Strukturos.h"
 bool SortByPazymys(Studentas stud1,Studentas stud2);
 bool SortByVardas(Studentas stud1,Studentas stud2);
@@ -7,3 +9,8 @@ void Console_ZinomasSk(std::vector<Studentas> &studentai, bool mediana, int m, i
 void Console_NezinomasSk(std::vector<Studentas> &studentai, bool mediana, int &m, int n);
 void Console_Random(std::vector<Studentas> &studentai, bool mediana, int m, int n);
 void File_Read(std::vector<Studentas> &studentai, std::ifstream &input, int &m, int n);
+void Analizuoti(std::vector<Studentas> studentai, int &m, int &n);
+void File_Read_List(std::list<Studentas> &studentai, std::ifstream &input, int &m, int n);
+void AnalizuotiList(std::list<Studentas> studentai, int &m, int &n);
+void File_Read_Deque(std::deque<Studentas> &studentai, std::ifstream &input, int &m, int n);
+void AnalizuotiDeque(std::deque<Studentas> studentai, int &m, int &n);
